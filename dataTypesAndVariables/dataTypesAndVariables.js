ab0ca868ff426f702f3c536de220e585872d886a
Yuliia Avrakhova <yuliia.avrakhova@globallogic.com>
2021-11-04 18:04:29 +0200

    first commit

--- dataTypesAndVariables/dataTypesAndVariables.js
+++ dataTypesAndVariables/dataTypesAndVariables.js
@@ -1,6 +1,6 @@
-
 //Data types and variables
 //primitive 
+
 const number = 3; //number;
 const MATH_PI = 3.14;//number;
 const myName = "Yuliia";//string;
@@ -13,6 +13,7 @@ let emptyVariable = null; //object;
 let temp; //undefined;
 let f = 0.0 //number;
 let g = "g"; // string;
+
 var boo = true; //boolean;
 console.log(typeof emptyVariable, typeof temp, typeof f, typeof g, typeof boo,); //checking data types
 
@@ -21,27 +22,27 @@ const cars = ["Audi", "Kia", "BMW"]; //array;
 
 const car = {type:"Audi", model:"B5", color:"black"}; //object;
 
-function myFunction() {             //function
+function myFunction() {     //function
     console.log ("Hello World!")
   };
-myFunction();
+myFunction(); //"Hello World!"
 
+//Operators
 // Assignment
 let h = 10;
-console.log(h);
+console.log(h);//10
 
 // Addition 
 let a = 6;
 let n = 8;
 let z = a + n;
-console.log(z);
+console.log(z);//14
 
 // Multiplying
 let a1 = 6;
 let n1 = 8;
 let z1 = a * n;
-console.log(z1);
-
+console.log(z1);//48
 
 // Compare operators;
 
@@ -63,64 +64,61 @@ const str4 = "String text";
 console.log (str3==str4);// Returns true
 console.log (str3===str4);// Returns true
 
-
-let result1 = 5 < 10;
+let result1 = 5 < 10;//true
 console.log(result1);
 
-let result2 = 5 > 10;
+let result2 = 5 > 10;//false
 console.log(result2);
 
-let result3 = 3 <= 10;
+let result3 = 3 <= 10;//true
 console.log(result3);
 
-let result4 = 3 >= 10;
+let result4 = 3 >= 10;//false
 console.log(result4);
-
-let result5 = 3 != 10;
+let result5 = 3 != 10;//true
 console.log(result5);
 
-// appropriation operators
+let result6 = 10 != 10;//false
+console.log(result6);
+
+// Appropriation operators
 let t1 = 5;
 t1 +=5;
-console.log(t1);
+console.log(t1);//10
 
 let t2 = 5;
 t2 -=5;
-console.log(t2);
+console.log(t2);//0
 
 let t3 = 5;
 t3 *=5;
-console.log(t3);
+console.log(t3);//25
 
 let t4 = 5;
 t4 /=5;
-console.log(t4);
+console.log(t4);//1
  
 let t5 = 5;
 t5 %=5;
-console.log(t5);
+console.log(t5);//0
 
 let t6 = 5;
 t6 **=5;
-console.log(t6);
+console.log(t6); //3125
 
-//String operators 
+// String operators 
 let userName = "Yuliia";
 let secondName = "Avrakhova";
 let fullName = userName + " " + secondName;
-console.log(fullName);
+console.log(fullName); // Yuliia Avrakhova
 
-let x = new String("John");
-let y = new String("John");
-console.log(x==y);//Comparing two JavaScript objects always returns false
+let userName1 = "Yuliia";
+let secondName1 = 1;
+let fullName1 = userName1 + " " + secondName1; 
+console.log(fullName1); // Yuliia 1
 
-let x1 = new String("Jay");
-let y1 = new String("Jay");
-console.log(x1===y1);//Comparing two JavaScript objects always returns false
-
-// I can add a comment like that
+// comment 
 
 /*
-  I can add a comment also like that
-  to have multilines comment 
+multilines comment 
 */
